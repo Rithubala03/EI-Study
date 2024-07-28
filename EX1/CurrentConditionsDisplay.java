610fe4d0ee045783bcf333da5fd310de40735e1f
Rithubala03 <rithubalaravisankar@gmail.com>
2024-07-28 03:33:32 -0700

    CurrentConditionsDisplay.java

---
+++ EX1/CurrentConditionsDisplay.java
@@ -0,0 +1,10 @@
+public class public class CurrentConditionsDisplay implements Observer {
+    public void update(float temperature, float humidity, float pressure) {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
+ implements Observer {
+    public void update(float temperature, float humidity, float pressure) {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
