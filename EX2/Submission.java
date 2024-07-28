32611a3f9e527f2fd3bf173fd862a62fc555c287
Rithubala03 <rithubalaravisankar@gmail.com>
2024-07-28 03:57:49 -0700

    Submission.java

---
+++ EX2/Submission.java
@@ -0,0 +1,11 @@
+public class Submission {
+    private String studentID;
+
+    public Submission(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+}
