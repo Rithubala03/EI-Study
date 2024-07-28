493814935cc4014c21b3d9093382a85a25a8ed95
Rithubala03 <rithubalaravisankar@gmail.com>
2024-07-28 03:34:51 -0700

    ExternalWeatherService.java

---
+++ EX1/ExternalWeatherService.java
@@ -0,0 +1,10 @@
+class class ExternalWeatherService {
+    public void getWeatherInfo() {
+        System.out.println("Getting weather information from external service...");
+    }
+}
+ {
+    public void getWeatherInfo() {
+        System.out.println("Getting weather information from external service...");
+    }
+}
